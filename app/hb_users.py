ab603e7ca12b800a2a7041b28b3174e01820c9b7
DanFaustine <danfaustine40@gmail.com>
2018-03-28 09:12:16 +0300

    Add changes to all files

--- app/hb_users.py
+++ app/hb_users.py
@@ -1,14 +1,14 @@
 user_list=[]
 
-class: hello_book_users
+class hello_book_users(): #call user
     def __init__(self):
         pass
 
-    def register_hello_book_users:
+    def register_hello_book_users():
         pass
 
-    def login_hb_users:
+    def login_hb_users():
         pass
 
-    def logout_hb_users:
+    def logout_hb_users():
         pass
